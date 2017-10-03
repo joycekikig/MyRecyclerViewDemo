b49df752c67a8f7295e4896c30a30bcb78ffca87
XiangYunLin <XiangYunLin@zeuscomm.com>
2017-10-03 16:49:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xiangyunlin/myrecyclerviewdemo/Model.java
@@ -0,0 +1,22 @@
+package com.example.xiangyunlin.myrecyclerviewdemo;
+
+/**
+ * Created by XiangYunLin on 2017/10/3.
+ */
+
+public class Model {
+    public static final int TEXT_TYPE = 0;
+    public static final int IMAGE_TYPE = 1;
+    public static final int AUDIO_TYPE = 2;
+
+    public int type;
+    public int data;
+    public String text;
+
+    public Model(int type, String text, int data)
+    {
+        this.type = type;
+        this.data = data;
+        this.text = text;
+    }
+}
